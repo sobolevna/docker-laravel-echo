2a6ecedcf1a0007ed58b4a37edb5c6e9e9658e33
sobolevna <sobolevna@sobolevna-W65-67SF>
2019-10-10 17:26:48 +0300

    Добавил скрипт для лёгкого входа в контейнер

---
+++ container.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+if [ ! -z $1 ]; then
+	if [[ $1 = 'postgres' ]] ; then 
+        docker exec -it $1 psql -U postgres -h postgres
+    elif docker exec -it $1 test -f /bin/bash; then
+		docker exec -it $1 bash
+	else
+		docker exec -it $1 sh
+	fi
+else
+	echo "No arg. Use $0 container_name "
+	exit 1
+fi
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Info 
+
+Your laravel project with laravel-echo-server.json should be at ./www directory
