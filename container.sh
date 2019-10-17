0709677fad060fcbddb2358975aad40e5f000044
sobolevna <sobolevna@sobolevna-W65-67SF>
2019-10-17 10:15:12 +0300

    Добавил вызов redis-cli при вызове контейнера redis

--- container.sh
+++ container.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 if [ ! -z $1 ]; then
-	if [[ $1 = 'postgres' ]] ; then 
+    if [[ $1 = 'postgres' ]] ; then 
         docker exec -it $1 psql -U postgres -h postgres
+    elif [[ $1 = 'redis' ]] ; then 
+        docker exec -it $1 redis-cli
     elif docker exec -it $1 test -f /bin/bash; then
 		docker exec -it $1 bash
 	else
