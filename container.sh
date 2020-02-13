da0851fe61a9c0cb853e30d2c0973b6cc5a5357d
sobolevna <sobolevna@sobolevna-W65-67SF>
2020-02-13 10:58:28 +0300

    Убрал имена контейнеров, чтобы можно было разворачивать независимое окружение когда охота

--- container.sh
+++ container.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-if [ ! -z $1 ]; then
+if [ ! -z $1 ]; then 
+    CONTAINER_ID="docker ps -q -f name=$1"
+    if [[ $($CONTAINER_ID) ]] ; then 
+        echo "Container ID: $($CONTAINER_ID)"
+    else 
+        echo "No container found: $1"
+        exit 1
+    fi
+
     if [[ $1 = 'postgres' ]] ; then 
-        docker exec -it $1 psql -U postgres -h postgres
+        docker exec -it $($CONTAINER_ID) psql -U postgres -h postgres
     elif [[ $1 = 'redis' ]] ; then 
-        docker exec -it $1 redis-cli
-    elif docker exec -it $1 test -f /bin/bash; then
-		docker exec -it $1 bash
+        docker exec -it $($CONTAINER_ID) redis-cli
+    elif docker exec -it $($CONTAINER_ID) test -f /bin/bash; then
+		docker exec -it $($CONTAINER_ID) bash
 	else
-		docker exec -it $1 sh
+		docker exec -it $($CONTAINER_ID) sh
 	fi
 else
 	echo "No arg. Use $0 container_name "
